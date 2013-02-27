d78f2e61269a6657d36431af652509955c377caf
Matthew Wilkes <git@matthewwilkes.name>
2013-02-27 12:05:43 +0000

    Improve trove classifiers

--- setup.py
+++ setup.py
@@ -74,6 +74,8 @@ setup(
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
         'Operating System :: OS Independent',
-        'Topic :: Software Development'
+        'Topic :: Software Development',
+        'Programming Language :: Python :: 2.4',
+        'Framework :: Zope2',
     ],
 )
